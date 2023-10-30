c77383d938a03de3ad91a94d5c14c438a2bdceb1
Gustavo Castanha <135674766+GustavoCastanha@users.noreply.github.com>
2023-10-29 23:16:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const buttons = document.querySelectorAll(".btn");
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const pressedButton = button.textContent;
+        let currentNumber = screen.textContent;
+
+        if (currentNumber === "Error!") {
+            currentNumber = "";
+        }
 
         if (button.id === "c") {
             screen.textContent = "0";
@@ -11,27 +16,39 @@ buttons.forEach(button => {
         }
 
         if (button.id === "delete") {
-            if (screen.textContent.length === 1 || screen.textContent === "Error!") {
+            if (currentNumber.length === 1 || currentNumber === "Error!") {
                 screen.textContent = "0";
             } else {
-                screen.textContent = screen.textContent.slice(0, -1);
+                currentNumber = currentNumber.slice(0, -1);
+                screen.textContent = currentNumber;
             }
             return;
         }
 
         if (button.id === "equals") {
             try {
-                screen.textContent = eval(screen.textContent);
+                const result = eval(currentNumber);
+                if (result.toString().length > 15) {
+                    screen.textContent = "+" + (result.toString().length - 15);
+                } else {
+                    screen.textContent = result;
+                }
             } catch {
                 screen.textContent = "Error!";
             }
             return;
         }
 
-        if (screen.textContent === "0" || screen.textContent === "Error!") {
-            screen.textContent = pressedButton;
-        } else {
-            screen.textContent += pressedButton;
+        if (currentNumber.length < 14) {
+            if (currentNumber === "0" || currentNumber === "Error!") {
+                currentNumber = pressedButton;
+            } else {
+                currentNumber += pressedButton;
+            }
+            screen.textContent = currentNumber;
+        } else if (currentNumber.length === 15) {
+            currentNumber += pressedButton;
+            screen.textContent = currentNumber;
         }
     });
 });
